4144e9973faa5362fb5bdff90305c9fa9d48b88d
sebas <sebastianpaez969@gmail.com>
2025-04-03 18:55:37 -0500

    mi segundo commit

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.10.0"
+  required_version = "~> 1.11.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,5 +13,42 @@ provider "aws" {
     role_arn     = var.assume_role_arn
     external_id  = var.assume_role_exteral_id
     session_name = "terraform"
+  }
+  default_tags {
+    tags = {
+      Environment = "Dev"
+      Proyecto    = "Pazgo"
+      AppId       = "123"
+    }
+  }
+}
+
+
+
+
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "cloud-state-703671936667"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-lock-703671936667"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
