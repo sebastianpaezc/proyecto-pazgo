23126a166a8d17900d006c9c80d496fbca53fbef
sebas <sebastianpaez969@gmail.com>
2025-04-03 22:03:30 -0500

    sebas

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "juanito" {
   ami                    = var.ami_id
   instance_type          = var.tipo_instancia
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = ["sg-0eaae6b3e421e56b4"]
   subnet_id              = var.subnet_id
 
   tags = {
@@ -13,7 +13,7 @@ resource "aws_instance" "juanito" {
 resource "aws_instance" "jorge" {
   ami                    = var.ami_id
   instance_type          = var.tipo_instancia
-  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_security_group_ids = ["sg-0eaae6b3e421e56b4"]
   subnet_id              = var.subnet_id
 
   tags = {
