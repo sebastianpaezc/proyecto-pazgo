20e84603953d309a12b6ef0f06ca28b50b0d4fb7
JhonnyEVT <s.jevt.net@hotmail.com>
2025-04-05 20:02:47 -0500

    Clonado de carpetas q

---
+++ env/demo/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "~> 1.11.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+  assume_role {
+    role_arn     = var.assume_role_arn
+    external_id  = var.assume_role_exteral_id
+    session_name = var.session_name
+  }
+  default_tags {
+    tags = {
+      Environment = var.entorno
+      Proyect     = var.proyect
+      AppId       = var.AppId
+    }
+  }
+}
